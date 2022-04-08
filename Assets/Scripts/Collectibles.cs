013d4deba18c1ab46c6713f589fe543c79179cee
CaptainHC12 <96395148+CaptainHC12@users.noreply.github.com>
2022-04-08 15:04:12 -0500

    Collectable

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+}
