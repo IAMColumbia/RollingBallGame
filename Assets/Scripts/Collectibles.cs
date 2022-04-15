1f458337b68a2efd92d606c3542ac7782d0cfd19
CaptainHC12 <hchillis@colum.edu>
2022-04-14 23:15:46 -0500

    The coins disappear when touched

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour
 {
-
+    // When the player touches the collectibles it will disappear
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coin"))
