aebff732b4f326c2b47d8acb48059b853b3a4b6b
CaptainHC12 <hchillis@colum.edu>
2022-04-22 13:59:33 -0500

    The coins can rotate

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour
 {
+    [SerializeField] float speedX;
+    [SerializeField] float speedY;
+    [SerializeField] float speedZ;
+
+
+
     // When the player touches the collectibles it will disappear
+    // Got this from a Youtube video https://www.youtube.com/watch?v=YQEq6Lkd69c
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coin"))
         {
             Destroy(other.gameObject);
         }
+    }
+
+    private void Update()
+    {
+        transform.Rotate(360 * speedX * Time.deltaTime, 360 * speedY * Time.deltaTime, 360 * speedZ * Time.deltaTime);
     }
 
 }
