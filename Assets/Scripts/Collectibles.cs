c970e6b363b6112faef220dbae37fee608163a45
CaptainHC12 <hchillis@colum.edu>
2022-04-28 23:14:11 -0500

    ScoreText was added and sound for coins

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Collectibles : MonoBehaviour
 {
+    //Got these examples from https://www.youtube.com/watch?v=Gs5QxGrRzNQ
+    int coins = 0;
+
     [SerializeField] float speedX;
     [SerializeField] float speedY;
     [SerializeField] float speedZ;
 
-
+    //Shoes text of the score
+    [SerializeField] Text scoreText;
+    //Plays sound of the coin being collected
+    [SerializeField] AudioSource collectionSound;
 
     // When the player touches the collectibles it will disappear
     // Got this from a Youtube video https://www.youtube.com/watch?v=YQEq6Lkd69c
@@ -17,11 +25,16 @@ public class Collectibles : MonoBehaviour
         if (other.gameObject.CompareTag("Coin"))
         {
             Destroy(other.gameObject);
+            coins++;
+            Debug.Log("Coins" + coins);
+            scoreText.text = ("Coins" + coins);
+            collectionSound.Play();
         }
     }
 
     private void Update()
     {
+        //Rotates the coin
         transform.Rotate(360 * speedX * Time.deltaTime, 360 * speedY * Time.deltaTime, 360 * speedZ * Time.deltaTime);
     }
 
