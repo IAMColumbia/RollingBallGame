6b2ee6ff0543ba878abffb5d2d65da9eefde9c5d
Hannah Stern <92752125+hannahkers@users.noreply.github.com>
2022-05-06 13:16:20 -0500

    Player Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Vector3 startingPoint;
     private Rigidbody rb;
+    public float bounds;
 
     private void FixedUpdate()
     {
@@ -21,7 +22,7 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movementDirection * speed * Time.deltaTime, ForceMode.VelocityChange);
 
-        if (transform.position.y < -15)
+        if (transform.position.y < startingPoint.y - bounds)
         {
             transform.position = startingPoint;
         }
@@ -31,26 +32,6 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Spin the object around the target at 20 degrees/second.
-        //transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
-        //how do i read input
-
-        //transform.Rotate
-
-        //float horizontalInput = Input.GetAxis("Horizontal");
-        //float verticalInput = Input.GetAxis("Vertical");
-
-        //Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        //movementDirection.Normalize();
-
-        //transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);   
-
-
     }
 
 }
